0141b9a392bdd3601388c0e30342c903c09e04e0
pluginmaster-coder <yujiniussmith@gmail.com>
2020-03-09 10:53:32 +0300

    gulpfile2

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,38 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sourcemaps = require ('gulp-sourcemaps');
-const sass = require('gulp-sass')
-// Static server
+const sass = require('gulp-sass');
 
-gulp.task('serve', function() {    
-    // serveSass();
+gulp.task('hello', function(done){
+console.log('Привет мир!');
+done();
+});
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Static server
+gulp.task('serve', function() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./src/"
         }
     });
-    gulp.watch("./src/*.html").on('change', browserSync.reload); 
-    gulp.watch("./src/sass/**/*.sass)", gulp.parallel ('serveSass'));   
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+    gulp.watch("./src/sass/**/*.sass)", serveSass());
     gulp.watch("./src/js/*.js").on("change", browserSync.reload);
 });
- gulp.task('serveSass', function () {
+//  gulp.task('', 
+function serveSass() {
           return gulp.src("./src/sass/*.sass")
         //   .pipe (sourcemaps.init())
           .pipe(sass().on('error',sass.logError))
         //   .pipe (sourcemaps.write(./))
           .pipe(gulp.dest("./src/css/"))
           .pipe(browserSync.stream());
-      });
-      
+      }      
       gulp.task('default');
 
 
-
+      
 // const {
 //     src,
 //     dest,
