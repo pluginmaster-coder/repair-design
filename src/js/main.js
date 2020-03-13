6b9c6250e4a2cfbab370a2d4bc8796dce30ed6f9
pluginmaster-coder <yujiniussmith@gmail.com>
2020-03-13 23:54:00 +0300

    feat: close modal window buttn,fons,esc  with js

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,26 @@
-// console.log('scrypt download')
-// $(document).ready(function () {
-  
-// });
-document.addEventListener("DOMContentLoaded", function(event) { 
-  //do work
+document.addEventListener("DOMContentLoaded", function ev(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector ('.modal__close');
+  // const closeWindow = 
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
-  }
-  // console.log(modalBtn);
-  
+    console.log('Клик по кнопке!');
+  };
+  document.addEventListener('click', e=> {
+    if (e.target == modal)
+    modal.classList.remove('modal--visible');   
+  });
+   document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    if (evt.keyCode == 27) {
+      modal.classList.remove('modal--visible');
+    }
+};
   modalBtn.forEach(element => {
     element.addEventListener('click', switchModal);
-    // console.log('Кликыч по кнопке!');
-    //вместо консоли выведем модальшое окно
   });
-
+  
   closeBtn.addEventListener('click', switchModal);
-
 
 }); 
